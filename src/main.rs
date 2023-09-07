b285c5ac5a2977bec3bd452a39948834dc37eeed
AlexSchunert <a.schunert@gmail.com>
2023-09-07 20:21:58 +0200

    replaced vec with hashmap, not buildable yet

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,83 @@
+use std::collections::HashMap;
+
 
 mod graph {
+    use std::collections::HashMap;
+
 
     pub struct Graph<'a>{
-        m_node_list: Vec<Node<'a>>,
-        m_edge_list: Vec<Edge>,
+        //m_node_list: Vec<Node<'a>>,
+        m_node_list: HashMap<u16, Node<'a>>,
+        m_edge_list: HashMap<u16, Edge>,
     }
 
     impl <'a>Graph<'a>{
 
         pub fn new() -> Graph<'a> {
-            return Graph{m_node_list: vec![], m_edge_list: vec![]};	
+            return Graph{m_node_list: HashMap::new(), m_edge_list:HashMap::new()};	
         }
 
         pub fn build_graph(&'a mut self) {
-        // Create nodes 
-        let mut node1: Node = Node::new(0);
-        let mut node2: Node = Node::new(1);
-        let mut node3: Node = Node::new(2);
+        // Create nodes
+        
+        let node_idx1: u16 = 1; 
+        let node_idx2: u16 = 2;
+        let node_idx3: u16 = 3;
+        let mut all_node_idxs =  vec![node_idx1,node_idx2,node_idx3];
+        let mut node1: Node = Node::new(node_idx1);
+        let mut node2: Node = Node::new(node_idx2);
+        let mut node3: Node = Node::new(node_idx3);
 
-        self.m_node_list.push(node1);
-        self.m_node_list.push(node2);
-        self.m_node_list.push(node3);
+        self.m_node_list.insert(node1.idx(), node1);
+        self.m_node_list.insert(node2.idx(), node2);
+        self.m_node_list.insert(node3.idx(), node3);
 
 
-        let mut edge12: Edge = Edge::new(0,self.m_node_list[0].idx(),self.m_node_list[1].idx());
-        let mut edge13: Edge = Edge::new(1,self.m_node_list[0].idx(),self.m_node_list[2].idx());
-        let mut edge23: Edge = Edge::new(2,self.m_node_list[1].idx(),self.m_node_list[2].idx());
+        let edge_idx1: u16 = 1; 
+        let edge_idx2: u16 = 2;
+        let edge_idx3: u16 = 3;
+        let mut all_edge_idxs =  vec![edge_idx1,edge_idx2,edge_idx3];
+        let mut edge12: Edge = Edge::new(0,node_idx1,node_idx2);
+        let mut edge13: Edge = Edge::new(1,node_idx1,node_idx3);
+        let mut edge23: Edge = Edge::new(2,node_idx2,node_idx3);
 
-        self.m_edge_list.push(edge12);
-        self.m_edge_list.push(edge13);
-        self.m_edge_list.push(edge23);
+        self.m_edge_list.insert(edge12.idx(),edge12);
+        self.m_edge_list.insert(edge13.idx(),edge13);
+        self.m_edge_list.insert(edge23.idx(),edge23);
 
         //let mut edge12: graph::Edge = graph::Edge::new(0);
         // Define nodes 
-        self.m_node_list[0].add_edge(&self.m_edge_list[0]);
-        self.m_node_list[1].add_edge(&self.m_edge_list[0]);
-        self.m_node_list[0].add_edge(&self.m_edge_list[1]);
-        self.m_node_list[2].add_edge(&self.m_edge_list[1]);
-        self.m_node_list[1].add_edge(&self.m_edge_list[2]);
-        self.m_node_list[2].add_edge(&self.m_edge_list[2]);
+        for idx in all_node_idxs.iter() {
+            match self.m_node_list.get_mut(&node_idx1) {
+                Some(x) => self.build_node_edge_list(x),
+                None => panic!("Idx {} unknown",node_idx1)
+            }
+
+        }
+    
+        //self.m_node_list[1].add_edge(&self.m_edge_list[0]);
+        //self.m_node_list[0].add_edge(&self.m_edge_list[1]);
+        //self.m_node_list[2].add_edge(&self.m_edge_list[1]);
+        //self.m_node_list[1].add_edge(&self.m_edge_list[2]);
+        //self.m_node_list[2].add_edge(&self.m_edge_list[2]);
 
         }
 
+
+        fn build_node_edge_list(&self ,node: &mut Node){
+            for (key,value) in self.m_edge_list{
+                if value.m_node_idx_end == node.idx() {
+                    node.add_edge(&value);
+                }
+                else if  value.m_node_idx_start == node.idx(){
+                    node.add_edge(&value);
+                }        
+
+            }
+
+        }
+
+        /*
         pub fn add_node(&mut self,node: Node<'a>) {
             self.m_node_list.push(node);
 
@@ -55,6 +91,7 @@ mod graph {
             self.m_node_list.sort_by(|a,b|a.m_idx.cmp(&b.m_idx));
 
         }
+         */
 
     }
 
@@ -91,6 +128,10 @@ mod graph {
         pub fn new(idx: u16, node_idx_start: u16, node_idx_end: u16) -> Edge{
 
             return Edge{m_idx: idx, m_node_idx_start: node_idx_start, m_node_idx_end:node_idx_end};
+        }
+
+        pub fn idx(&self) -> u16 {
+            return self.m_idx;
         }
 
     }
